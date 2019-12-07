a774e0d9d2eeb0ef324f486f4b13e5284960133c
benirudi <58515838+benirudi@users.noreply.github.com>
2019-12-07 23:11:18 +0100

    Add files via upload

---
+++ Modul_Prüfung/script/script.js
@@ -0,0 +1,6 @@
+/* Habe diesen Code von unserer Lektion am 4.12.19 genommen */
+$("#barcontainer").click(function(){
+    $(this).toggleClass("change");
+    $("#responsivenav").toggleClass("responsive");
+})
+/* Code ende */
